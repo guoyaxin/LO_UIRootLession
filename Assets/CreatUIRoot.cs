e69a6de90b28710d6bd615c70326973cd3679e89
chenlitao <269391704@qq.com>
2015-08-29 19:42:14 +0800

    first commit

---
+++ Assets/CreatUIRoot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatUIRoot : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		CreatRoot ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void CreatRoot ()
+	{
+		UIPanel panl = NGUITools.CreateUI (false);
+		UIRoot root = panl.GetComponent<UIRoot> ();
+		root.scalingStyle = UIRoot.Scaling.Flexible;
+		root.minimumHeight = 320;
+		root.maximumHeight = 960;
+		root.shrinkPortraitUI = true;
+		root.adjustByDPI = true;
+
+	}
+}
